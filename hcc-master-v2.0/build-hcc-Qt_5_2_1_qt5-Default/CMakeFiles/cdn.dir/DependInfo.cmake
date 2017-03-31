2028062e1a3aa00363412eb43c1835dd79d3ec30
LooperZheng <zhengliupo@163.com>
2017-03-31 20:08:44 +0800

    Add files via upload

---
+++ hcc-master-v2.0/build-hcc-Qt_5_2_1_qt5-Default/CMakeFiles/cdn.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zlp/hcc-master/hcc/cdn.cpp" "/home/zlp/hcc-master/build-hcc-Qt_5_2_1_qt5-Default/CMakeFiles/cdn.dir/cdn.cpp.o"
+  "/home/zlp/hcc-master/hcc/deploy.cpp" "/home/zlp/hcc-master/build-hcc-Qt_5_2_1_qt5-Default/CMakeFiles/cdn.dir/deploy.cpp.o"
+  "/home/zlp/hcc-master/hcc/io.cpp" "/home/zlp/hcc-master/build-hcc-Qt_5_2_1_qt5-Default/CMakeFiles/cdn.dir/io.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/zlp/hcc-master/hcc/lib"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
